9886b3c12453b2ac047b7f33fbd3ea44f118c5d7
ftmanspile <codfan15@gmail.com>
2018-01-10 09:05:06 -0500

    broken

--- app.rb
+++ app.rb
@@ -12,6 +12,7 @@ post '/shells' do
 end
 
 get '/meat' do
+	shells = params[:shells]
 	erb :meat, :locals => {:shells => shells}
 end
 
@@ -22,23 +23,36 @@ post '/meat' do
 end
  
 get '/cheese' do
-	erb :cheese, :locals => {:shells => shells, :meat_choice => meat_choice}
+    shells = params[:shells]
+    meat_choice = params[:meat_choice]
+    erb :cheese, :locals => {:shells => shells, :meat_choice => meat_choice}
 end
 
 post'/cheese' do
-	shells = params[:shells]
-	meat_choice =  params[:meat_choice]
-	cheese_choice = params[:cheese_choice]
-	puts "Thank you for your order"
-	# redirect 'veggie?shells=' + shells + '&meat_choice' + meat_choice + '&cheese_choice' + cheese_choice
+    shells = params[:shells]
+    meat_choice =  params[:meat_choice]
+    cheese_choice = params[:cheese_choice]
+    redirect 'veggie?shells=' + shells + '&meat_choice' + meat_choice + '&cheese_choice' + cheese_choice
 end
 
-# get '/veggie' do
-# 	erb :veggie
-# end
+get '/veggie' do
+	shells = params[:shells]
+	meat_choice = params[:meat_choice]
+	cheese_choice = params[:cheese_choice]
+	erb :veggie, :locals => {:shells => shells, :meat_choice => meat_choice, :cheese_choice => cheese_choice}
+end
+
+post '/veggie' do
+	shells = params[:shells]
+	meat_choice = params[:meat_choice]
+	cheese_choice = params[:cheese_choice]
+	veggie_choice = params[:veggie_choice]
+	redirect 'sauce?shells=' + shells +'&meat_choice' + meat_choice + '&cheese_choice' + cheese_choice + '&veggie_choice' + veggie_choice
+end
 
 # get '/sauce' do
 # 	erb :firstedit
 # end
+
 
 
